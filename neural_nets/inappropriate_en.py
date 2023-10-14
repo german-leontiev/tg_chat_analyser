e85c7be9325e68273bd5610397a4d26daa1bf498
Герман Леонтьев <gleontev@web-bee.ru>
2023-10-14 11:13:35 +0300

    NSFW filter added

---
+++ neural_nets/inappropriate_en.py
@@ -0,0 +1,16 @@
+import torch
+from transformers import pipeline
+
+
+model_folder_path = "michellejieli/inappropriate_text_classifier"
+
+
+classifier = pipeline("sentiment-analysis", model="michellejieli/inappropriate_text_classifier")
+ 
+
+# Probabilistic prediction of emotion in a text
+@torch.no_grad()
+def predict_appropriateness(text: str) -> list:
+    pred = classifier(text)
+    result = pred['score'] if pred["label"] != "NSFW" else 1 - pred["score"]
+    return result
